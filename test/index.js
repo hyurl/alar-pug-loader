62555e7e12b56387840721a4d4dbd00023463712
Ayon Lee <ayonlys@gmail.com>
2020-01-14 01:35:51 +0800

    update alar to v6

--- test/index.js
+++ test/index.js
@@ -6,6 +6,6 @@ var view = new alar.ModuleProxy("views", __dirname);
 
 view.setLoader(new PugLoader());
 
-assert.strictEqual(view.hello.instance().render({ name: "Timothy" }), "<p>Timothy's Pug source code!</p>");
+assert.strictEqual(view.hello().render({ name: "Timothy" }), "<p>Timothy's Pug source code!</p>");
 
 console.log("#### OK ####");
